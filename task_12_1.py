14f43342dd83a7175a017794285f8bd16f1d7ae6
kuzzzko <kuzzzko@gmail.com>
2019-03-15 15:10:24 -0400

    commit

--- task_12_1.py
+++ task_12_1.py
@@ -1,21 +1,21 @@
 #!/usr/bin/env python3.6
 
 import subprocess
+from tabulate import tabulate
 
 def ping_ip(ip_addr_list):
     '''
     Ping IP address and return two lists:
     available and unavailable addresses
     '''
-    available = []
-    unavailable = []
+    reach, unreach = [],[]
     for ip_addr in ip_addr_list:
         reply = subprocess.run(['ping','-c','3','-n','-W','1',ip_addr], stdout=subprocess.DEVNULL)
         if reply.returncode == 0:
-            available.append(ip_addr)
+            reach.append(ip_addr)
         else:
-            unavailable.append(ip_addr)
-    return available, unavailable
+            unreach.append(ip_addr)
+    return reach, unreach
 
 def check_ip_range(ip_addr_list):
     '''
@@ -31,14 +31,36 @@ def check_ip_range(ip_addr_list):
                 result.append(ip_rng + '.' + str(i))
         else:
             result.append(ip_addr)
-    print(result)
     return(result)
+
+def ip_table(available,unavailable):
+    '''
+    Print IP table reach and unreach
+    input:
+    2 list of ip addresses
+    '''
+    header = ['Reachable','Unreachable']
+    result = []
+    if len(available) > len(unavailable):
+        temp_list = unavailable.copy()
+        for item in range(0,len(available) - len(unavailable)):
+            temp_list.append(' ')
+        for item in range(0,len(available)):
+            result.append([available[item],temp_list[item]])
+    elif len(available) < len(unavailable):
+        temp_list = available.copy()
+        for item in range(0,len(unavailable) - len(available)):
+            temp_list.append(' ')
+        for item in range(0,len(unavailable)):
+            result.append([temp_list[item],unavailable[item]])
+    else:
+        for item in range(0,len(available)):
+            result.append([available[item],unavailable[item]])
+    print(tabulate(result,headers=header))
 
 
 ip_list = ['8.8.8.8','8.9.8.9','10.0.0.1','192.168.1.1','192.168.0.1','8.8.8.1-10']
 ip_list_2 = ['8.9.8.1-10','8.8.8.8','192.168.1.1-192.168.1.10']
 
-ok, not_ok = ping_ip(check_ip_range(ip_list_2))
-
-print(ok)
-print(not_ok)
+reach, unreach = ping_ip(check_ip_range(ip_list))
+ip_table(reach,unreach)
