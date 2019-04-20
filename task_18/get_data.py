6be15e9f53a26b8305e3b91eaf97963fff676383
kuzzzko <kuzzzko@gmail.com>
2019-04-20 22:40:31 +0300

    commit

--- task_18/get_data.py
+++ task_18/get_data.py
@@ -3,19 +3,15 @@
 import sqlite3
 import sys
 
-def output_with_2args(key,value,keys,cursor_obj):
-    print('\nDetailed information for host(s) with', key, value)
-    print('-' * 40)
+def output_with_2args(keys,cursor_obj):
     for row in cursor_obj:
         for k in keys:
             print('{:12}: {}'.format(k, row[k]))
         print('-' * 40)
 
 def output_with_0args(cursor_obj):
-    print('\nВ таблице dhcp такие записи:')
-    print('-' * 73)
     for row in cursor_obj:
-        print('{:20}  {:15}  {:5}  {:18}  {}  {}'.format(*row))
+        print('{:20}  {:15}  {:5}  {:18}  {}'.format(*row))
 
 db_filename = 'dhcp_snooping.db'
 conn = sqlite3.connect(db_filename)
@@ -29,11 +25,24 @@ if len(sys.argv) == 3:
     else:
         keys.remove(key)
         conn.row_factory = sqlite3.Row
-        query = 'select * from dhcp where {} = ?'.format(key)
-        result = conn.execute(query, (value, ))
-        output_with_2args(key,value,keys,result)
+        print('\nDetailed information for host(s) with', key, value)
+        print('-' * 40)
+        query_act = 'select * from dhcp where {} = ? and active = 1'.format(key)
+        output_with_2args(keys,conn.execute(query_act, (value, )))
+        print('\n' + '=' * 40)
+        print('\nInactive values:')
+        print('-' * 40)
+        query_inact = 'select * from dhcp where {} = ? and active = 0'.format(key)
+        output_with_2args(keys, conn.execute(query_inact, (value, )))
 elif len(sys.argv) == 1:
-    output_with_0args(conn.execute('select * from dhcp'))
+    print('-' * 70)
+    print('Active values:')
+    print('-' * 70)
+    output_with_0args(conn.execute('select * from dhcp where active = 1'))
+    print('-' * 70)
+    print('Inactive values:')
+    print('-' * 70)
+    output_with_0args(conn.execute('select * from dhcp where active = 0'))
 else:
     print('Пожалуйста, введите два или ноль аргументов')
 
